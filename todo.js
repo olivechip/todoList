667ec427c39bb89f4c71d335d8dd7a627b333df6
Oliver <oliver@MSI>
2022-12-28 18:42:55 -0800

    css edits

--- todo.js
+++ todo.js
@@ -45,7 +45,7 @@ function showTasks(){
   }
   let newLiTag = "";
   listArray.forEach((element, index) => {
-    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="add-trash"></i></span></li>`;
+    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"></span></li>`;
   });
   todoList.innerHTML = newLiTag; 
   inputBox.value = ""; 
